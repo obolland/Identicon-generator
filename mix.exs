3f2c83c99e6b78a9a2785cad16dcefa41f3e09a6
OllyB <olly.bolland@statsbomb.com>
2022-07-24 17:32:50 +0200

    Elixir application to draw and save an identicon image from a string

---
+++ mix.exs
@@ -0,0 +1,28 @@
+defmodule Identicon.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :identicon,
+      version: "0.1.0",
+      elixir: "~> 1.13",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger, :crypto],
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:egd, github: "erlang/egd"},
+      {:ex_doc, "~> 0.12"}
+    ]
+  end
+end
